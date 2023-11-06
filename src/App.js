4533c8838ff05db8d17f9fef0c96a751a006a96d
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-07 01:33:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
-import PricingCol from '.components/pricecards/PriceCard';
+import PriceCard from '.components/pricecards/PriceCard';
 
 
 
