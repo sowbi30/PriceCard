48994238f5e7080af48ba558c385516d472b5902
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-06 16:25:51 +0530

    Update PriceCard.jsx

--- src/components/pricecards/PriceCard.jsx
+++ src/components/pricecards/PriceCard.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import <FontAwesomeIcon icon="fa-regular fa-check" />
-import <FontAwesomeIcon icon="fa-solid fa-xmark" />
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faCheck as faCheckRegular } from '@fortawesome/free-regular-svg-icons';
 import './Style.css';
 
-function PriceCard() {
-  return(
-
-<section className="pricing py-5">
+function Pricecard() {
+  return (
+    <section className="pricing py-5">
       <div className="container">
         <div className="row">
           <div className="col-lg-4">
@@ -17,14 +16,14 @@ function PriceCard() {
                 <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
                 <hr />
                 <ul className="fa-ul">
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>Single User</li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>5GB Storage</li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>Community Access</li>
-                  <li className="text-muted"><span className="fa-li"><i className="fa-solid fa-xmark"></i></span>Unlimited Private Projects</li>
-                  <li className="text-muted"><span className="fa-li"><i className="fa-solid fa-xmark"></i></span>Dedicated Phone Support</li>
-                  <li className="text-muted"><span className="fa-li"><i className="fa-solid fa-xmark"></i></span>Free Subdomain</li>
-                  <li className="text-muted"><span className="fa-li"><i className="fa-solid fa-xmark"></i></span>Monthly Status Reports</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Single User</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>5GB Storage</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Public Projects</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Community Access</li>
+                  <li className="text-muted"><span className="fa-li"><FontAwesomeIcon icon={faTimes} /></span>Unlimited Private Projects</li>
+                  <li className="text-muted"><span className="fa-li"><FontAwesomeIcon icon={faTimes} /></span>Dedicated Phone Support</li>
+                  <li className="text-muted"><span className="fa-li"><FontAwesomeIcon icon={faTimes} /></span>Free Subdomain</li>
+                  <li className="text-muted"><span className="fa-li"><FontAwesomeIcon icon={faTimes} /></span>Monthly Status Reports</li>
                 </ul>
                 <div className="d-grid">
                   <a href="#" className="btn btn-primary text-uppercase">Button</a>
@@ -32,7 +31,7 @@ function PriceCard() {
               </div>
             </div>
           </div>
-     
+
           <div className="col-lg-4">
             <div className="card mb-5 mb-lg-0">
               <div className="card-body">
@@ -40,14 +39,14 @@ function PriceCard() {
                 <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
                 <hr />
                 <ul className="fa-ul">
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span><strong>5 Users</strong></li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>50GB Storage</li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>Community Access</li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>Unlimited Private Projects</li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>Dedicated Phone Support</li>
-                  <li><span className="fa-li"><i className="fa-regular fa-check"></i></span>Free Subdomain</li>
-                  <li className="text-muted"><span className="fa-li"><i className="fa-solid fa-xmark"></i></span>Monthly Status Reports</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span><strong>5 Users</strong></li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>50GB Storage</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Public Projects</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Community Access</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Private Projects</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Dedicated Phone Support</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Free Subdomain</li>
+                  <li className="text-muted"><span className="fa-li"><FontAwesomeIcon icon={faTimes} /></span>Monthly Status Reports</li>
                 </ul>
                 <div className="d-grid">
                   <a href="#" className="btn btn-primary text-uppercase">Button</a>
@@ -63,14 +62,14 @@ function PriceCard() {
                 <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
                 <hr />
                 <ul className="fa-ul">
-                  <li><span className="fa-li"><i className="fas fa-checkfa-regular fa-check"></i></span><strong>Unlimited Users</strong></li>
-                  <li><span className="fa-li"><i className="fas fa-checkfa-regular fa-check"></i></span>150GB Storage</li>
-                  <li><span className="fa-li"><i className="fas fa-checkfa-regular fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span className="fa-li"><i className="fas fa-checkfa-regular fa-check"></i></span>Community Access</li>
-                  <li><span className="fa-li"><i className="fas fa-checkfa-regular fa-check"></i></span>Unlimited Private Projects</li>
-                  <li><span className="fa-li"><i className="fas fa-checkfa-regular fa-check"></i></span>Dedicated Phone Support</li>
-                  <li><span className="fa-li"><i className="fas fa-checkfa-regular fa-check"></i></span><strong>Unlimited</strong> Free Subdomains</li>
-                  <li><span className="fa-li"><i className="fas fa-checfa-regular fa-check"></i></span>Monthly Status Reports</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /><FontAwesomeIcon icon={faCheckRegular} /></span><strong>Unlimited Users</strong></li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /><FontAwesomeIcon icon={faCheckRegular} /></span>150GB Storage</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /><FontAwesomeIcon icon={faCheckRegular} /></span>Unlimited Public Projects</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /><FontAwesomeIcon icon={faCheckRegular} /></span>Community Access</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /><FontAwesomeIcon icon={faCheckRegular} /></span>Unlimited Private Projects</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /><FontAwesomeIcon icon={faCheckRegular} /></span>Dedicated Phone Support</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /><FontAwesomeIcon icon={faCheckRegular} /></span><strong>Unlimited</strong> Free Subdomains</li>
+                  <li><span className="fa-li"><FontAwesomeIcon icon={faCheckRegular} /></span>Monthly Status Reports</li>
                 </ul>
                 <div className="d-grid">
                   <a href="#" className="btn btn-primary text-uppercase">Button</a>
@@ -82,5 +81,6 @@ function PriceCard() {
       </div>
     </section>
   );
+}
 
-  export default PriceCard;
+export default Pricecard;
