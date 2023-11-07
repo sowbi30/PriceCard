d003cc3d9713453f5c566c89791d60fea9c09bdd
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-08 01:23:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,75 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.min.css';
-import PriceCard from '.components/pricecards/PriceCard';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PriceCard from "./PriceCard";
 
-import React from 'react';
-import './App.css';
-import Card from './Card';
 
 function App() {
-  const plandetails = [
-    {
-      price: 0,
-      planName: 'FREE',
-      users: 'Single User',
-      gb: '10GB Storage',
-      Publicproject: '10 Public Projects',
-      access: 'Limited Access',
-      project: '2 Private Projects',
-      support: 'Email Support',
-      domain: 'No Subdomain',
-      reports: 'Monthly Status Reports',
-    },
-    {
-      price: 9,
-      planName: 'PLUS',
-      users: 'Multiple Users',
-      gb: '50GB Storage',
-      Publicproject: 'Unlimited Public Projects',
-      access: 'Full Access',
-      project: '10 Private Projects',
-      support: 'Phone Support',
-      domain: 'Custom Domain',
-      reports: 'Weekly Status Reports',
-    },
-    {
-      price: 49,
-      planName: 'PRO',
-      users: 'Single User',
-      gb: '5GB Storage',
-      Publicproject: 'Unlimited Public Projects',
-      access: 'Limited Access',
-      project: '1 Private Project',
-      support: 'No Support',
-      domain: 'Free Subdomain',
-      reports: 'No Reports',
-    },
-  ];
+  const Plandetails = {
+      "free": [
+        {"id":"0", "title":"Single User", "availability": true, "iconSize": "5x" },
+        {"id":"1", "title":"5GB Storage", "availability": true,"iconSize": "5x" },
+        {"id":"2", "title":"Unlimited Public Projects", "availability": true ,"iconSize": "5x" },
+        {"id":"3", "title":"Community Access", "availability": true ,"iconSize": "5x" },
+        {"id":"4", "title":"Unlimited Private Projects", "availability": false,"iconSize": "5x"  },
+        {"id":"5", "title":"Dedicated Phone Support", "availability": false,"iconSize": "5x"  },
+        {"id":"6", "title":"Free Subdomain", "availability": false,"iconSize": "5x"  },
+        {"id":"7", "title":"Monthly Status Reports", "availability": false,"iconSize": "5x"  },
+      ],
+      "plus": [
+        {"id":"8", "title":"5 Users", "availability": true,"iconSize": "5x"  },
+        {"id":"9", "title":"50GB Storage", "availability": true,"iconSize": "5x"  },
+        {"id":"10", "title":"Unlimited Public Projects", "availability": true,"iconSize": "5x"  },
+        {"id":"11", "title":"Community Access", "availability": true,"iconSize": "5x"  },
+        {"id":"12", "title":"Unlimited Private Projects", "availability": true,"iconSize": "5x"  },
+        {"id":"13", "title":"Dedicated Phone Support", "availability": true ,"iconSize": "5x" },
+        {"id":"14", "title":"Free Subdomain", "availability": true,"iconSize": "5x"  },
+        {"id":"15", "title":"Monthly Status Reports", "availability": false ,"iconSize": "5x" },
+      ],
+      "pro": [
+        {"id":"16", "title":"Unlimited Users", "availability": true,"iconSize": "5x"  },
+        {"id":"17", "title":"150GB Storage", "availability": true ,"iconSize": "5x" },
+        {"id":"18", "title":"Unlimited Public Projects", "availability": true ,"iconSize": "5x" },
+        {"id":"19", "title":"Community Access", "availability": true,"iconSize": "5x"  },
+        {"id":"20", "title":"Unlimited Private Projects", "availability": true,"iconSize": "5x" },
+        {"id":"21", "title":"Dedicated Phone Support", "availability": true,"iconSize": "5x"  },
+        {"id":"22", "title":"Free Subdomain", "availability": true ,"iconSize": "5x" },
+        {"id":"23", "title":"Monthly Status Reports", "availability": true,"iconSize": "5x"  },
+      ]
+
+  };
+
 
   return (
-    <div className='page'>
-    <section class="pricing py-5">
-    <div className="container">
-      <div className="row">
-        <div className="col">
-        </div>
-      </div>
-      <div className="row">
-        {plandetails.map((details, index) => (
-          <Card details={details} key={index} />
-        ))}
-      </div>
-    </div>
-    </section>
+    <div className="App">
+        <section className="pricing py-5">
+          <div className="container">
+              <div className="row">
+                  {/* <!-- Free Tier --> */}
+                  <PriceCard
+                    tier = { "Free" }
+                    pricing = { 0 }
+                    features = { Plandetails["free"] }
+                  />
+                  <priceCard
+                    tier = { "Plus" }
+                    pricing = { 200 }
+                    features = { Plandetails["plus"] }
+                  />
+                  <PriceCard 
+                    tier = { "Pro" }
+                    pricing = { 800 }
+                    features = { Plandetails["pro"] }
+                  />
+              </div> 
+             
+          </div>
+          
+</section>
     </div>
   );
 }
 
 export default App;
 
-
+ 
