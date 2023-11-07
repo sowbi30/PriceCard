bd00b2286b2e540e9a64c08ca89d3c347e4cebe1
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-07 20:41:43 +0530

    Update Feature.jsx

--- src/components/pricecards/Feature.jsx
+++ src/components/pricecards/Feature.jsx
@@ -1,19 +1,2 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
+..
 
-function Feature({title, availability}){
-    // Choose Icon based on Feature availability
-    let featureIcon = availability ? faCheck : faXmark;
-    // Choose to fade text based on Feature availability
-    let featureClass = availability ? "" : "text-muted";
-    return (
-        <li class={featureClass}>
-            <span className="fa-li">
-                <FontAwesomeIcon icon={featureIcon} />
-            </span>
-            {title}
-        </li>
-    )
-}
-
-export default Feature;
