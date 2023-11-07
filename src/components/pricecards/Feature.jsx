b4c0cc8db9b1c9c6f2bc91cd8ff74280df1599c0
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-07 23:57:47 +0530

    Update Feature.jsx

--- src/components/pricecards/Feature.jsx
+++ src/components/pricecards/Feature.jsx
@@ -1,2 +1,19 @@
-..
-
+ function Feature{
+   return(
+   <ul className="fa-ul">
+              {details.features.map((item) => {
+                return (
+                  <li className={item.availability ? '' : 'text-muted'}>
+                    <span className="fa-li">
+                      {item.availability ? (
+                        <FontAwesomeIcon icon={faCheck} />
+                      ) : (
+                        <FontAwesomeIcon icon={faXmark} />
+                      )}
+                    </span>
+                    {item.access}
+                  </li>
+                  )
+   };
+                                    
+export default Feature;
