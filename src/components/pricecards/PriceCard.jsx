26423601354a8683f830d85bd4f0aca5c4142cf4
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-08 00:29:39 +0530

    Update PriceCard.jsx

--- src/components/pricecards/PriceCard.jsx
+++ src/components/pricecards/PriceCard.jsx
@@ -1,36 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import FeatureItem from "./FeatureItem";
+import Feature from "./Feature";
+ function Card ({tier, pricing, features}){
+      return (
+          <div className="col-lg-4">
 
-function Card(){      
-const Card = ({ details }) => {
-  return (
-    <div className="col-lg-4 col-md-8">
-      
-      <div className="card mb-5 mb-lg-0">
-        
-        <div className="card-body">
-          
-          <h5 className="card-title text-muted text-uppercase text-center">{details.title}</h5>
-          
-          <h6 className="card-price text-center">${details.pack}<span className="period">/month</span></h6>
-          
-          <hr />
-          
-          
-                );
-              })}
-            </ul>
-            <div className="d-grid">
-              <button className="btn btn-primary text-uppercase">Button</button>
-            </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+              <div className="card mb-5 mb-lg-0">
 
+                  <div className="card-body">
 
+                      <h5 className="card-title text-muted text-uppercase text-center">{tier}</h5>
+                      
+                      <h6 className="card-price text-center">
 
-export default Card;
+                          ${pricing}<span className="period">/month</span>
+                      </h6>
+                      <hr />
+                      <ul className="fa-ul">
+                          {features.map((feature) => {
+                              console.log(feature);
+                              return (
+                              <Feature title={feature.title} availability={feature.availability} key={feature.id}/>
+                             ) 
+                          })}
+                      </ul>
+                      <div className="d-grid">
+                          <a className="btn btn-primary text-uppercase">Button</a>
+                      </div>
+                  </div>
+              </div> 
+          </div>
+      )
+  
+  }
+  
+  
+  
+  export default PriceCard;
