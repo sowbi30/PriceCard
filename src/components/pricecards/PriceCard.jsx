b233fe25484d87a512cda89aef50baf5c0c50005
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-11-07 20:40:41 +0530

    Update PriceCard.jsx

--- src/components/pricecards/PriceCard.jsx
+++ src/components/pricecards/PriceCard.jsx
@@ -1,37 +1,34 @@
 import React from 'react'
 import Feature from ".components/pricecards/Feature";
 
-function PriceCard({tier, pricing, features}){
+function PriceCard({ details }) {
   return (
-      <div className="col-lg-4">
-          <div className="card mb-5 mb-lg-0">
-              <div className="card-body">
-                  <h5 className="card-title text-muted text-uppercase text-center">{tier}</h5>
-                  <h6 className="card-price text-center">
-                      ${pricing}<span className="period">/month</span>
-                  </h6>
-                  <hr />
-                  <ul className="fa-ul">
-                      {/* Create the list of features */}
-                      {features.map((feature) => {
-                          console.log(feature);
-                          return (
-                          <Feature 
-                              title={feature.title} 
-                              availability={feature.availability} 
-                              key={feature.id}
-                          />
-                         ) 
-                      })}
-                  </ul>
-                  <div className="d-grid">
-                      <a href="#" className="btn btn-primary text-uppercase">Button</a>
-                  </div>
-              </div>
-          </div> 
+    <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+        <header>
+        <h6 className="card-title text-muted text-uppercase text-center">{details.planName}</h6>
+        <h1 className="card-price text-center"><span className="period">${details.price}<sub>/month</sub></span></h1>
+        </header>
+        <ul className="fa-ul">
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.users}</li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.gb}</li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.Publicproject}</li>
+          <li><span className="fa-li"><i className="fas fa-check"></i></span>{details.access}</li>
+          <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.project}</li>
+          <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.support}</li>
+          <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.domain}</li>
+          <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>{details.reports}</li>
+        </ul>
+        <div className="d-grid">
+          <a className="btn btn-primary text-uppercase">Button</a>
+        </div>
       </div>
-  )
-
+      /</div>
+    </div>
+  );
 }
 
   export default PriceCard;
+
+
